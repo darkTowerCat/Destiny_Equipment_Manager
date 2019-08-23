61da9b543e151ff8ccb82fa13b239ccd6f8f00a6
Terry <tet2@pdx.edu>
2019-08-23 10:40:01 -0700

    before refactor

--- destiny.rb
+++ destiny.rb
@@ -1,16 +1,22 @@
+#!/usr/bin/env ruby
 require "net/http"
+require "json"
 
-require "uri"
-
+player_name = "cortical_iv"
+platform = "2"
 
 #get api from external file
 file = File.open("api-key.txt")
 api_key = file.read.strip
 file.close
 
-# get user member id
+def get_json(uri, api_key)
+	
+	
+# get player member id
 # https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/2/cortical_iv/
-uri = URI("https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/2/cortical_iv/")
+player_search = "https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/" + platform + "/" + player_name +"/"
+uri = URI(player_search)
 req = Net::HTTP::Get.new(uri)
 req['X-API-Key'] = api_key
 
@@ -18,11 +24,22 @@ response = Net::HTTP.start(uri.hostname, uri.port, opt = {use_ssl: true}) {
 	|http|
 	http.request(req)
 }
-#puts res.body
+# puts response.body
+parsed = JSON.parse(response.body)
+player_id = parsed["Response"][0]["membershipId"]
 
 # get equipment list
 #https://www.bungie.net/Platform/Destiny2/2/Profile/4611686018459314819/?components=205
+gear_search = "https://www.bungie.net/Platform/Destiny2/" + platform + "/Profile/" + player_id + "/?components=205"
+uri = URI(gear_search)
+req = Net::HTTP::Get.new(uri)
+req['X-API-Key'] = api_key
 
+response = Net::HTTP.start(uri.hostname, uri.port, opt = {use_ssl: true}) {
+	|http|
+	http.request(req)
+}
+puts response.body
 #unmangle if needed
 #https://www.bungie.net/Platform/Destiny2/Manifest/DestinyInventoryItemDefinition/1345867571
 
